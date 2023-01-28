3692efbacbb4431fdb1ba1d6999207b9fff1b47c
nazimudheen267 <nazimudheen267@gmail.com>
2023-01-28 15:30:54 +0530

    drawer content created

--- src/Navigators/Drawer.js
+++ src/Navigators/Drawer.js
@@ -1,14 +1,27 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { 
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList
+} from "@react-navigation/drawer";
+import { Text } from "react-native";
+import Register from "../Screens/Registers/Register";
 
 import BottomTab from "./BottomTab" // navigator component
 
 const { Navigator, Screen } = createDrawerNavigator();
 
 const DrawerScreens = () => (
-    <Navigator screenOptions={{headerShown: false}}>
+    <Navigator screenOptions={{headerShown: true}} drawerContent={DrawerContents}>
         <Screen name="BottomTab" component={BottomTab} />
+        <Screen name="Register" component={Register} />
     </Navigator>
-)
+);
+
+function DrawerContents(props) {
+    return (
+        
+    )
+}
 
 
 export default DrawerScreens;
---
+++ src/Navigators/Style/DrawerContent.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const DrawerHeader = styled.View`
+    height: 200px;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Profile = styled.View`
+    flex-direction: "row";
+`;
+
+export const ProfileImg = styled.Image`
+    border-width: 2px;
+    border-color: green;
+    border-radius: 50px;
+    height: 100px;
+    width: 100px;
+`;
+
