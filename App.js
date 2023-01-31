14c80434d998e4e3cac1d5138f5b5c413f5e2f8a
nazimudheen267 <nazimudheen267@gmail.com>
2023-02-01 02:01:23 +0530

    splash added

--- App.js
+++ App.js
@@ -2,13 +2,54 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { StackNavigator } from "./src/Navigators/Stack";
+import styled from 'styled-components/native';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  function hideSplash() {
+    setIsVisible(false);
+  }
+
+  useEffect(() => {
+    setTimeout(hideSplash, 4000);
+  }, [])
+
+  const SplashScreen = (
+    <ParentSplash>
+      <ChildSplash>
+        <Image source={require("./src/Assets/Images/logo.png")} />
+      </ChildSplash>
+    </ParentSplash>
+  );
+
   return (
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
+      isVisible ? SplashScreen : (
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      )
   )
 }
 
-export default App
+export default App;
+
+const ParentSplash = styled.View`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  flex: 1;
+`;
+
+const ChildSplash = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: yellow;
+`;
+
+const Image = styled.Image`
+  height: 60px;
+  width: 60px;
+`;
